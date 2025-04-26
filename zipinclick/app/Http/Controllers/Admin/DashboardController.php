d80807cf5cff148d807bcafcacbd0ec0be2015db
Taoufik <silverproduction2023@gmail.com>
2025-04-26 11:30:49 +0200

    complete user register and login with new template

--- zipinclick/app/Http/Controllers/Admin/DashboardController.php
+++ zipinclick/app/Http/Controllers/Admin/DashboardController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
 
 
-    public function index()
+    public function index():View
     {
-       return view('admin.dashboard.index');
+        return view('frontend.dashboard.index');
     }
 
 
--- zipinclick/routes/web.php
+++ zipinclick/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -9,9 +10,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[HomeController::class, 'index'])->name('home');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::Group(['middleware'=>['auth','verified']], function () {
+    Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
+});
 
 
 
